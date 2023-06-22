1e99ce98d8519088856d9e467151e21bc04c8964
justinsegundo <134711597+justinsegundo@users.noreply.github.com>
2023-06-22 06:59:16 -0700

    new changes1

--- base/views.py
+++ base/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 # Create your views here.
+
 def home(request):
-    return render(request, 'base/home.html')
+    return render(request, "base/home.html")
 
 def about(request):
-    return render(request, 'base/about.html')
+    return render(request, "base/about.html")
 
 def services(request):
-    return render(request, 'base/services.html')
+    return render(request, "base/services.html")
 
 def contact(request):
-    return render(request, 'base/contact.html')
+    return render(request, "base/contact.html")
